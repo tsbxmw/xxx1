494c461c07110110727797a63972625c9f3549aa
mengwei <meng.wei@zte.com.cn>
2016-11-22 15:21:50 +0800

    add judgement of MainActivity , the userstring and the passstring is allowed to be digital and letter (A-Z a-z 0-9)

--- app/src/main/java/com/example/app_test/MainActivity.java
+++ app/src/main/java/com/example/app_test/MainActivity.java
@@ -1,30 +1,27 @@
 package com.example.app_test;
 
 
-import java.security.MessageDigest;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import ServerConnect.GetInfo;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends Activity {
 
-	private EditText user=null;
-	private EditText pass=null;
+	private EditText user = null;
+	private EditText pass = null;
 	private Button button_back = null;
 	private Button button_login = null;
 	private Button button_register = null;
@@ -72,6 +69,21 @@ public class MainActivity extends Activity {
 	public void LoginNow(final View view){
 		String userString = user.getText().toString();
 		String passString = pass.getText().toString();
+		if(isLetterOrDigit(userString))
+			System.out.println();
+		else {
+			Intent intent = new Intent(MainActivity.this,MainActivity.class);
+			startActivity(intent);
+			this.finish();
+		}
+		if(isLetterDigit(passString))
+			System.out.println();
+		else{
+			Intent intent = new Intent(MainActivity.this,MainActivity.class);
+			startActivity(intent);
+			this.finish();
+		}
+
 
 		Context context = MainActivity.this;
 	    SharedPreferences sp = context.getSharedPreferences("Login", MODE_PRIVATE);
@@ -106,5 +118,96 @@ public class MainActivity extends Activity {
 		super.onBackPressed();
 		this.finish();
 	}
-	
+
+
+
+	/**
+	 * 规则1：至少包含大小写字母及数字中的一种
+	 * 是否包含
+	 *
+	 * @param str
+	 * @return
+	 */
+	public static boolean isLetterOrDigit(String str) {
+		boolean isLetterOrDigit = false;//定义一个boolean值，用来表示是否包含字母或数字
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isLetterOrDigit(str.charAt(i))) {   //用char包装类中的判断数字的方法判断每一个字符
+				isLetterOrDigit = true;
+			}
+		}
+		String regex = "^[a-zA-Z0-9]+$";
+		boolean isRight = isLetterOrDigit && str.matches(regex);
+		return isRight;
+	}
+
+	/**
+	 * 规则2：至少包含大小写字母及数字中的两种
+	 * 是否包含
+	 *
+	 * @param str
+	 * @return
+	 */
+	public static boolean isLetterDigit(String str) {
+		boolean isDigit = false;//定义一个boolean值，用来表示是否包含数字
+		boolean isLetter = false;//定义一个boolean值，用来表示是否包含字母
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isDigit(str.charAt(i))) {   //用char包装类中的判断数字的方法判断每一个字符
+				isDigit = true;
+			} else if (Character.isLetter(str.charAt(i))) {  //用char包装类中的判断字母的方法判断每一个字符
+				isLetter = true;
+			}
+		}
+		String regex = "^[a-zA-Z0-9]+$";
+		boolean isRight = isDigit && isLetter && str.matches(regex);
+		return isRight;
+	}
+
+	/**
+	 * 规则3：必须同时包含大小写字母及数字
+	 * 是否包含
+	 *
+	 * @param str
+	 * @return
+	 */
+	public static boolean isContainAll(String str) {
+		boolean isDigit = false;//定义一个boolean值，用来表示是否包含数字
+		boolean isLowerCase = false;//定义一个boolean值，用来表示是否包含字母
+		boolean isUpperCase = false;
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isDigit(str.charAt(i))) {   //用char包装类中的判断数字的方法判断每一个字符
+				isDigit = true;
+			} else if (Character.isLowerCase(str.charAt(i))) {  //用char包装类中的判断字母的方法判断每一个字符
+				isLowerCase = true;
+			} else if (Character.isUpperCase(str.charAt(i))) {
+				isUpperCase = true;
+			}
+		}
+		String regex = "^[a-zA-Z0-9]+$";
+		boolean isRight = isDigit && isLowerCase && isUpperCase && str.matches(regex);
+		return isRight;
+	}
+
+	/**
+	 *   判断EditText输入的数字、中文还是字母方法
+	 */
+	public static void whatIsInput(Context context, EditText edInput) {
+		String txt = edInput.getText().toString();
+
+		Pattern p = Pattern.compile("[0-9]*");
+		Matcher m = p.matcher(txt);
+		if (m.matches()) {
+			Toast.makeText(context, "输入的是数字", Toast.LENGTH_SHORT).show();
+		}
+		p = Pattern.compile("[a-zA-Z]");
+		m = p.matcher(txt);
+		if (m.matches()) {
+			Toast.makeText(context, "输入的是字母", Toast.LENGTH_SHORT).show();
+		}
+		p = Pattern.compile("[\u4e00-\u9fa5]");
+		m = p.matcher(txt);
+		if (m.matches()) {
+			Toast.makeText(context, "输入的是汉字", Toast.LENGTH_SHORT).show();
+		}
+	}
 }
+
